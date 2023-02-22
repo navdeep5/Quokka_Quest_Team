6467b85775057ed0cebe4109686bcf12b2e61f69
Youngwoo Choi <cyan246010@gmail.com>
2023-02-21 21:42:05 -0700

    Added custom textbox with bust

--- cmput250 game_test/js/plugins.js
+++ cmput250 game_test/js/plugins.js
@@ -5,5 +5,7 @@ var $plugins =
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
 {"name":"DMY_SkipVideo","status":true,"description":"Skip video on key press","parameters":{"Disabler switch":"0"}},
-{"name":"YEP_SaveEventLocations","status":true,"description":"v1.06 Enable specified maps to memorize the locations of\nevents when leaving and loading them upon reentering map.","parameters":{}}
+{"name":"YEP_SaveEventLocations","status":true,"description":"v1.06 Enable specified maps to memorize the locations of\nevents when leaving and loading them upon reentering map.","parameters":{}},
+{"name":"GALV_MessageBusts","status":true,"description":"(v.2.8) Displays a bust image instead of selected face image","parameters":{"Bust Priority":"1","Bust Position":"1","Text X Offset":"275","Fade Out Speed":"32","Filename Append":""}},
+{"name":"GALV_MessageBackground","status":true,"description":"(v.2.0) Displays an image behind messages in place of the windowskin","parameters":{"Image Variable ID":"1","Disable Switch ID":"1"}}
 ];
